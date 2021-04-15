2459b216eb7e9a48c33284818bd57b275d23ec9b
Loi Nguyen <loi.nguyen@friartuck.net>
2021-04-15 12:29:56 +0800

    Minor updates

--- src/main/java/com/loi/loanproposal/module/service/SendEscalationNoticeService.java
+++ src/main/java/com/loi/loanproposal/module/service/SendEscalationNoticeService.java
@@ -1,6 +1,6 @@
 package com.loi.loanproposal.module.service;
 
-import com.loi.loanproposal.configuration.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
@@ -10,13 +10,11 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component("sendEscalationNotice")
+@Slf4j
 public class SendEscalationNoticeService implements JavaDelegate {
-
-	private final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(Logger.class.getName());
-	
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		LOGGER.info("Sent escalation notice");
+		log.info("Sent escalation notice");
 	}
 
 }
