22e994ae5fef27d8601a679df69bc627c7155840
Loi Nguyen <thangloi2501@gmail.com>
2021-04-26 22:57:35 +0800

    Fix camunda version 7.15, error: rest api get variables 404

--- src/main/java/com/loi/loanproposal/configuration/WebMvcConfig.java
+++ src/main/java/com/loi/loanproposal/configuration/WebMvcConfig.java
@@ -1,24 +1,24 @@
-package com.loi.loanproposal.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author Loi Nguyen
- */
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final long MAX_AGE_SECS = 3600;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
-}
+//package com.loi.loanproposal.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author Loi Nguyen
+// */
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    private final long MAX_AGE_SECS = 3600;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
+//}
