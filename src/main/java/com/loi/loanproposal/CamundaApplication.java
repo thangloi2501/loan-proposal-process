8ab08b0b6a993e6cf7deeac808074c807fd4fcda
Loi Nguyen <loi.nguyen@friartuck.net>
2021-06-18 16:57:01 +0800

    test travis

--- src/main/java/com/loi/loanproposal/CamundaApplication.java
+++ src/main/java/com/loi/loanproposal/CamundaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableProcessApplication("loan-proposal-process")
+/**
+ * @author Loi Nguyen
+ */
 public class CamundaApplication{
 	public static void main(String... args) {
 		SpringApplication.run(CamundaApplication.class, args);
